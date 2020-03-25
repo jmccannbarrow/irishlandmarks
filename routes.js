2da3d6a3e0696dea497b9f146c23f807e32d13ea
jmccannwit <johnmccann02@yahoo.co.uk>
2020-03-25 16:21:20 +0000

    Update changes to signup/login and related pages

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const Landmarks = require('./app/controllers/landmarks');
 
 module.exports = [
     { method: 'GET', path: '/', config: Landmarks.index },
+
+    { method: 'GET', path: '/signup', config: Landmarks.signup },
+    { method: 'GET', path: '/login', config: Landmarks.login },
     {
         method: 'GET',
         path: '/{param*}',
